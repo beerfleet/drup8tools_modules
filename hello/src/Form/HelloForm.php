8b7b64170f9cbd6a6e88193621d58c5c29320351
Jan Van Biervliet <jan.van.biervliet@etion.be>
2019-09-17 16:26:48 +0200

    Full form built

--- hello/src/Form/HelloForm.php
+++ hello/src/Form/HelloForm.php
@@ -20,12 +20,158 @@ class HelloForm extends FormBase {
 
   //put your code here
   public function buildForm(array $form, FormStateInterface $form_state): array {
+
+    // textfield
     $form['job_title'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Job Title'),
       '#description' => $this->t('Enter your job title. At least 5 chars in length, please, kind sir. Thank you. Come again.'),
       '#required' => TRUE,
     );
+
+    // checkboxes
+    $form['tests_taken'] = [
+      '#type' => 'checkboxes',
+      '#options' => ['SAT' => $this->t('SAT'), 'ACT' => t('ACT')],
+      '#title' => $this->t('What standardized tests did you take ?'),
+      '#description' => $this->t('No tests taken = no checkboxes checked.'),
+    ];
+
+    // Color.
+    $form['color'] = [
+      '#type' => 'color',
+      '#title' => $this->t('Color'),
+      '#default_value' => '#ffffff',
+      '#description' => $this->t('Pick a color by clicking on the color above'),
+    ];
+
+    // Date
+    $form['email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Email'),
+      '#description' => $this->t('Enter your email address'),
+    ];
+
+    // Number
+    $form['quantity'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Quantity'),
+      '#description' => $this->t('Enter a number, any number'),
+    ];
+
+    // field set
+    $form['password'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Password'),
+    ];
+
+    // Password
+    $form['password']['field'] = [
+      '#type' => 'password',
+      '#title' => $this->t('Old Password'),
+      '#description' => $this->t('Enter a password')
+    ];
+
+    // Password confirm
+    $form['password']['field_confirm'] = [
+      '#type' => 'password_confirm',
+      '#title' => $this->t('Confirm password'),
+      '#description' => $this->t('Confirm your password.'),
+    ];
+
+    // Range
+    $form['size'] = [
+      '#type' => 'range',
+      '#title' => $this->t('Size'),
+      '#min' => 10,
+      '#max' => 100,
+      '#description' => $this->t('Slider control. Ole ole'),
+    ];
+
+    // Radios
+    $form['settings']['active'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Poll status'),
+      '#options' => [0 => $this->t('Open'), 1 => $this->t('Closed')],
+      '#description' => $this->t('Select Open or closed'),
+    ];
+
+    //Search
+    $form['search'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Search Group'),
+    ];
+
+    $form['search']['search'] = [
+      '#type' => 'search',
+      '#title' => $this->t('Search'),
+      '#description' => $this->t('Search something'),
+    ];
+
+    $form['search']['favorite'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Favorite color'),
+      '#options' => [
+        'red' => $this->t('Red'),
+        'yellow' => $this->t('Yellow'),
+        'blue' => $this->t('Blue'),
+      ],
+      '#empty_option' => $this->t('-select-'),
+      '#description' => $this->t('My favorite color is: '),
+    ];
+
+    // tel
+    $form['search']['tel'] = [
+      '#type' => 'tel',
+      '#title' => $this->t('Phone'),
+      '#description' => $this->t('Enter phony number.'),
+    ];
+
+    // tableSelect
+    $options = [
+      1 => ['first_name' => 'Indy', 'last_name' => 'Jones'],
+      2 => ['first_name' => 'Piercing', 'last_name' => 'Brosnandus'],
+      1 => ['first_name' => 'Agathaar', 'last_name' => 'Queestie'],
+    ];
+
+    $header = [
+      'first_name' => $this->t('First Name'),
+      'last_name' => $this->t('Last Name'),
+    ];
+
+    $form['table'] = [
+      '#type' => 'tableselect',
+      '#title' => $this->t('Users'),
+      '#title_display' => 'visible',
+      '#header' => $header,
+      '#options' => $options,
+      '#empty' => $this->t('No users found'),
+    ];
+
+    $form['texts'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Some textboxes here'),
+    ];
+
+    $form['texts']['text'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Text'),
+      '#description' => $this->t('A whole lot of letters can go here.'),
+    ];
+
+    $form['texts']['subject'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Subject'),
+      '#size' => 60,
+      '#ùaxlength' => 128,
+      '#description' => $this->t('Just another text field'),
+    ];
+
+    $form['texts']['weight'] = [
+      '#type' => 'weight',
+      '#title' => $this->t('Weight'),
+      '#description' => $this->t('Drupal weight control.'),
+    ];
 
     $form['actions'] = array(
       '#type' => 'actions',
@@ -35,6 +181,9 @@ class HelloForm extends FormBase {
       '#type' => 'submit',
       '#value' => $this->t('Submit'),
     );
+
+    $form['actions']['submit']['#attributes']['class'][] = 'btn-block';
+
 
     return $form;
   }
