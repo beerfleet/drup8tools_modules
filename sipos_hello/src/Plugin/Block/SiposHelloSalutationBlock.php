7c30c608863ac9a1546b83063ac307c5ed8fa322
Jan Van Biervliet <jan.van.biervliet@etion.be>
2019-10-02 16:16:04 +0200

    someadjustments to block - still not working

--- sipos_hello/src/Plugin/Block/SiposHelloSalutationBlock.php
+++ sipos_hello/src/Plugin/Block/SiposHelloSalutationBlock.php
@@ -6,6 +6,7 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\sipos_hello\SiposHelloSalutation as SiposHelloSalutationService;
+use Drupal\Core\Form\FormStateInterface;
 
 /* THIS ANNOTAITON DENOTES THAT THIS CLASS IS A BLOCK PLUGIN */
 
@@ -15,7 +16,7 @@ use Drupal\sipos_hello\SiposHelloSalutation as SiposHelloSalutationService;
  * @Block(
  *   id = "sipos_hello_salutation_block",
  *   admin_label = @Translation("Sipos Hello Salutation"),
- *   category = @Translation("Hello World"),
+ *   category = @Translation("Sipos Hello"),
  * )
  */
 class SiposHelloSalutationBlock extends BlockBase implements ContainerFactoryPluginInterface {
@@ -29,14 +30,13 @@ class SiposHelloSalutationBlock extends BlockBase implements ContainerFactoryPlu
    * Construct.
    *
    * @param array $configuration
-   * A configuration array containing information about the plugin
-    instance.
+   * A configuration array containing information about the plugin instance.
    * @param string $plugin_id
    * The plugin_id for the plugin instance.
    * @param string $plugin_definition
    * The plugin implementation definition.
-   * @param \Drupal\hello_world\HelloWorldSalutation $salutation * The
-    salutation service.
+   * @param \Drupal\hello_world\SiposHelloSalutation $salutation 
+   * The salutation service.
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, SiposHelloSalutationService $salutation) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
@@ -59,6 +59,28 @@ class SiposHelloSalutationBlock extends BlockBase implements ContainerFactoryPlu
     return [
       '#markup' => $this->salutation->getSalutation(),
     ];
+  }
+
+  public function defaultConfiguration() {
+    return [
+      'enabled' => 1,
+    ];
+  }
+
+  public function blockForm($form, FormStateInterface $form_state) {
+    $config = $this->getConfiguration();
+
+    $form['enabled'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Enabled'),
+      '#description' => t('Check this box if you want to enable this feature.'),
+      '#default_value' => $config['enabled'],
+    );
+    return $form;
+  }
+
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['enabled'] = $form_state->getValue('enabled');
   }
 
 }
