51003919beb961ad22f4b2947005b833285d196c
Jan Van Biervliet <jan.van.biervliet@etion.be>
2019-10-22 11:29:27 +0200

    Add logger functionality

---
+++ sipos_hello/src/Logger/SiposMailLogger.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Sipos custom mail logger
+ */
+
+namespace Drupal\hello_world\Logger;
+
+use Drupal\Core\Logger\RfcLoggerTrait;
+use Psr\Log\LoggerInterface;
+
+/**
+ * SiposMailLogger
+ *
+ * @author jvanbiervliet
+ */
+class SiposMailLogger {
+  //put your code here
+}
