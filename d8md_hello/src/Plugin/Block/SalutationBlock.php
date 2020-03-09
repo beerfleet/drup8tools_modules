4eb0a9fcf1585f613119ec11790f6d52fd848661
Jan Van Biervliet <jvbiervliet@gmail.com>
2020-03-09 22:45:21 +0100

    salutation block

---
+++ d8md_hello/src/Plugin/Block/SalutationBlock.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Drupal\d8md_hello\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\d8md_hello\HiSalutation;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\hello_world\HiSalutation as HiSalutationService;
+
+/**
+ * D8md_hello Salutation block.
+ *
+ * @Block(
+ * id = "d8md_hello_salutation_block",
+ * admin_label = @Translation("Hi salutation"),
+ * )
+ */
+class HelloWorldSalutationBlock extends BlockBase implements
+ContainerFactoryPluginInterface {
+
+  /**
+   * The salutation service.
+   *
+   * @var \Drupal\d8md_hello\HiSalutation
+   */
+  protected $salutation;
+
+  /**
+   * Construct.
+   *
+   * @param array $configuration
+   * A configuration array containing information about the plugin
+    instance.
+   * @param string $plugin_id
+   * The plugin_id for the plugin instance.
+   * @param string $plugin_definition
+   * The plugin implementation definition.
+   * @param \Drupal\d8md_hello\HiSalutation $salutation * The
+    salutation service.
+   */
+  public function __construct(array $configuration, $plugin_id,
+    $plugin_definition, HiSalutationService $salutation) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->salutation = $salutation;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array
+    $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('d8md_hello.hi')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#markup' => $this->salutation->getSalutation(),
+    ];
+  }
+
+}
