d4fd2f22f5c7295b1ef3c2b282cc30dd14f17096
Jan Van Biervliet <jan.van.biervliet@etion.be>
2020-03-12 16:18:39 +0100

    Sick my duck forms

--- oef_raadgetal/src/Form/RaadGetalForm.php
+++ oef_raadgetal/src/Form/RaadGetalForm.php
@@ -12,17 +12,20 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class RaadGetalForm extends FormBase {
 
+  /**
+   * {@inheritdoc}
+   */
   public function buildForm(array $form, FormStateInterface $form_state): array {
     $form['#theme'] = 'oef_raadgetal_raad_form';
     $form['getal'] = [
       '#type' => 'number',
       '#title' => $this->t('Getal')
     ];
-    
+
     $form['actions'] = [
       '#type' => 'actions',
     ];
-    
+
     $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Submit'),
@@ -31,12 +34,30 @@ class RaadGetalForm extends FormBase {
     return $form;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function getFormId(): string {
     return 'oef_raadgetal_form';
   }
 
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    foreach ($form_state->getValues() as $key => $value) {
+      // @TODO: Validate fields.
+    }
+    parent::validateForm($form, $form_state);
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Display result.
+    foreach ($form_state->getValues() as $key => $value) {
+      \Drupal::messenger()->addMessage($key . ': ' . ($key === 'text_format' ? $value['value'] : $value));
+    }
+  }
 }
