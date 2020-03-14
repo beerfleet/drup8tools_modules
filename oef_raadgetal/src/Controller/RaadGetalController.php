e6f6f023931f7ad32a2e8fb3de7237b0f1a3d9a2
Jan Van Biervliet <jan.van.biervliet@etion.be>
2020-03-14 16:35:55 +0100

    const value references

--- oef_raadgetal/src/Controller/RaadGetalController.php
+++ oef_raadgetal/src/Controller/RaadGetalController.php
@@ -16,8 +16,21 @@ use \Drupal\Core\Controller\ControllerBase;
  * @author jvanbiervliet
  */
 class RaadGetalController extends ControllerBase {
+  
+  const GETAL = "raad_getal_waarde";
+  const POGINGEN = "raad_getal_pogingen";
+
+  function init_state() {
+    $this->set_random_number();
+  }
+
+  function set_random_number() {
+    \Drupal::state()->set(self::GETAL, rand(1,10));
+    \Drupal::state()->set(self::POGINGEN, 3);
+  }
 
   function start() {
+    $this->init_state();
     $form = \Drupal::formBuilder()->getForm('Drupal\oef_raadgetal\Form\RaadGetalForm');
     $build = [
       '#theme' => 'oef_raadgetal_start_pagina',
